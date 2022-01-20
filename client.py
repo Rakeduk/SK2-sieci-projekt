0d9c4846cfaf5e41533ac8cfb1c9a97279439ef7
Rakeduk <69343181+Rakeduk@users.noreply.github.com>
2022-01-20 19:33:29 +0100

    Non blocking client
    100% cpu usage

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-from tkinter import * 
+from tkinter import *
 from time import sleep
 import fcntl, os
 import errno
@@ -11,20 +11,18 @@ ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 
-def main():    
+
+def main():
     #########========GRAPHIC PART=============################
     root = Tk()
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)  
+    client.connect(ADDR)
     fcntl.fcntl(client, fcntl.F_SETFL, os.O_NONBLOCK)
 
-    def connectAndGetFirstCards ():
-        """ TCP Socket """
-        
-
+    def connectAndGetFirstCards():
         """ Send login """
-        login = myEntry.get() 
+        login = myEntry.get()
         client.send(login.encode(FORMAT))
 
         # """ Not blocking socket """
@@ -32,7 +30,6 @@ def main():
             try:
                 data = client.recv(SIZE).decode(FORMAT)
             except socket.error as e:
-                print("Waiting for data")
                 err = e.args[0]
                 if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
                     root.update()
@@ -41,10 +38,10 @@ def main():
                     # a "real" error occurred
                     print(e)
                     sys.exit(1)
-            else:                
+            else:
                 print(f"[SERVER] {data}")
                 res = data.split()
-                if res[0] == '0':
+                if res[0] == "0":
                     lab5.config(text=str(res[1]))
                     lab6.config(text=str(res[2]))
                     lab7.config(text=str(res[3]))
@@ -54,43 +51,43 @@ def main():
                     lab2.config(text=str(res[1]))
                     lab3.config(text=str(res[2]))
                     root.update()
-                
-        
+
         """ Close connection """
-        #client.close()
+        # client.close()
 
     def doNothingYet():
-        myAnswer = myEntry.get() 
-        client.send(myAnswer.encode(FORMAT))
+        myCard = [lab5['text'], lab6['text'], lab7['text']]
+        myAnswer = myEntry.get()
+        if myAnswer in myCard:
+            client.send(myAnswer.encode(FORMAT))
+        else:
+            print("Wrong input")
 
     root.geometry("1000x300")
-    lab1 = Label(root,text="Position 1", width=30)
+    lab1 = Label(root, text="Position 1", width=30)
     lab1.pack()
 
-    lab2 = Label(root,text="Position 1", width=30)
+    lab2 = Label(root, text="Position 1", width=30)
     lab2.pack()
 
-    lab3 = Label(root,text="Position 1", width=30)
+    lab3 = Label(root, text="Position 1", width=30)
     lab3.pack()
 
-    spacer_lab = Label(root,text="", width=30)
+    spacer_lab = Label(root, text="", width=30)
     spacer_lab.pack()
 
-    lab4 = Label(root,text="A to twoje karta!", width=30)
+    lab4 = Label(root, text="A to twoje karta!", width=30)
     lab4.pack()
 
-    lab5 = Label(root,text="Position 1", width=30)
+    lab5 = Label(root, text="Position 1", width=30)
     lab5.pack()
 
-    lab6 = Label(root,text="Position 1", width=30)
+    lab6 = Label(root, text="Position 1", width=30)
     lab6.pack()
-    lab7 = Label(root,text="Position 1", width=30)
+    lab7 = Label(root, text="Position 1", width=30)
     lab7.pack()
 
-    
-
-
-    myEntry = Entry(root,width = 30)
+    myEntry = Entry(root, width=30)
     myEntry.pack()
 
     myButton = Button(root, text="Send answer!", command=doNothingYet)
@@ -98,11 +95,9 @@ def main():
 
     myConnectButton = Button(root, text="Connect me!", command=connectAndGetFirstCards)
     myConnectButton.pack()
-        
-        
+
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
-
-
