4d6bddcc5216da17b252e5ed67c0999d73a6609f
Rakeduk <69343181+Rakeduk@users.noreply.github.com>
2022-01-22 00:26:35 +0100

    Blocking socket with thread

--- client.py
+++ client.py
@@ -1,103 +1,167 @@
+import tkinter as tk
+from tkinter import messagebox
 import socket
-from tkinter import *
-from time import sleep
-import fcntl, os
-import errno
+import threading
+
+# network client
+client = None
+HOST_ADDR = "192.168.1.3"
+HOST_PORT = 4456
+
+window = tk.Tk()
+window.title("Client")
+username = " "
 
 
-IP = "127.0.0.1"
-PORT = 4456
-ADDR = (IP, PORT)
-SIZE = 1024
-FORMAT = "utf-8"
+topFrame = tk.Frame(window)
+lblName = tk.Label(topFrame, text = "Name:").pack(side=tk.LEFT)
+entName = tk.Entry(topFrame)
+entName.pack(side=tk.LEFT)
+btnConnect = tk.Button(topFrame, text="Connect", command=lambda : connect())
+btnConnect.pack(side=tk.LEFT)
+#btnConnect.bind('<Button-1>', connect)
+topFrame.pack(side=tk.TOP)
 
 
-def main():
-    #########========GRAPHIC PART=============################
-    root = Tk()
+middleFrame = tk.Frame(window)  
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    fcntl.fcntl(client, fcntl.F_SETFL, os.O_NONBLOCK)
+allCardsFrame = tk.Frame(middleFrame)
 
-    def connectAndGetFirstCards():
-        """ Send login """
-        login = myEntry.get()
-        client.send(login.encode(FORMAT))
+mainCardFrame = tk.Frame(allCardsFrame)
+lblLine = tk.Label(mainCardFrame, text="*********************************************************************").pack()
+lab1 = tk.Label(mainCardFrame, text="first element", width=20, height = 10)
+lab1.pack(side=tk.LEFT)
+lab2 = tk.Label(mainCardFrame, text="second element", width=20, height = 10)
+lab2.pack(side=tk.LEFT)
+lab3 = tk.Label(mainCardFrame, text="third element", width=20, height = 10)
+lab3.pack(side=tk.LEFT)
+mainCardFrame.pack(side=tk.TOP)
 
-        # """ Not blocking socket """
-        while True:
-            try:
-                data = client.recv(SIZE).decode(FORMAT)
-            except socket.error as e:
-                err = e.args[0]
-                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-                    root.update()
-                    continue
+myCardsFrame = tk.Frame(allCardsFrame)
+lblLine = tk.Label(myCardsFrame, text="***Your cards!***").pack()
+lab5 = tk.Label(myCardsFrame, text="first card", width=20, height = 10)
+lab5.pack(side=tk.LEFT)
+lab6 = tk.Label(myCardsFrame, text="second second", width=20, height = 10)
+lab6.pack(side=tk.LEFT)
+lab7 = tk.Label(myCardsFrame, text="third card", width=20, height =10)
+lab7.pack(side=tk.LEFT)
+myCardsFrame.pack(side=tk.TOP)
+
+allCardsFrame.pack(side=tk.LEFT)
+
+    
+rankingFrame = tk.Frame(middleFrame)
+tkDisplay = tk.Text(rankingFrame, height=20, width = 10)
+tkDisplay.pack(side=tk.LEFT, fill=tk.X)
+tkDisplay.tag_config("tag_your_message", foreground="blue")
+rankingFrame.pack(side=tk.LEFT)
+
+middleFrame.pack(side=tk.TOP)
+
+
+bottomFrame = tk.Frame(window)
+tkMessage = tk.Text(bottomFrame, height=2, width=40)
+tkMessage.pack(side=tk.LEFT, padx=(5, 13), pady=(5, 10))
+tkMessage.config(highlightbackground="grey", state="disabled")
+tkMessage.bind("<Return>", (lambda event: getChatMessage(tkMessage.get("1.0", tk.END))))
+bottomFrame.pack(side=tk.BOTTOM)
+
+
+def connect():
+    global username, client
+    if len(entName.get()) < 1:
+        tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name <e.g. John>")
+    else:
+        username = entName.get()
+        connect_to_server(username)
+
+
+
+
+def connect_to_server(name):
+    global client, HOST_PORT, HOST_ADDR
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST_ADDR, HOST_PORT))
+        client.send(name.encode()) # Send name to server after connecting
+
+        entName.config(state=tk.DISABLED)
+        btnConnect.config(state=tk.DISABLED)
+        tkMessage.config(state=tk.NORMAL)
+
+        # start a thread to keep receiving message from server
+        # do not block the main thread :)
+        threading._start_new_thread(receive_message_from_server, (client, "m"))
+    except Exception as e:
+        tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be Unavailable. Try again later")
+
+
+def receive_message_from_server(sck, m):
+    while True:
+        from_server = sck.recv(64).decode()
+
+        if not from_server: break
+
+        print(f"[SERVER] {from_server}")
+        res = from_server.split()
+        if res[0] == "0":
+            lab5.config(text=str(res[1]))
+            lab6.config(text=str(res[2]))
+            lab7.config(text=str(res[3]))
+            window.update()
+        elif res[0] == "1":
+            del res[0]
+            print(res)
+            tkDisplay.config(state=tk.NORMAL)
+            texts = tkDisplay.delete("1.0", tk.END)
+
+            for i in range(len(res)):
+                if i%2 == 0:
+                    print("nieparzysta" + res[i])
+                    tkDisplay.insert(tk.END, res[i] + " ")
                 else:
-                    # a "real" error occurred
-                    print(e)
-                    sys.exit(1)
-            else:
-                print(f"[SERVER] {data}")
-                res = data.split()
-                if res[0] == "0":
-                    lab5.config(text=str(res[1]))
-                    lab6.config(text=str(res[2]))
-                    lab7.config(text=str(res[3]))
-                    root.update()
-                else:
-                    lab1.config(text=str(res[0]))
-                    lab2.config(text=str(res[1]))
-                    lab3.config(text=str(res[2]))
-                    root.update()
+                    tkDisplay.insert(tk.END, res[i] + "\n")
 
-        """ Close connection """
-        # client.close()
-
-    def doNothingYet():
-        myCard = [lab5['text'], lab6['text'], lab7['text']]
-        myAnswer = myEntry.get()
-        if myAnswer in myCard:
-            client.send(myAnswer.encode(FORMAT))
+            tkDisplay.config(state=tk.DISABLED)
+            tkDisplay.see(tk.END)
         else:
-            print("Wrong input")
+            lab1.config(text=str(res[0]))
+            lab2.config(text=str(res[1]))
+            lab3.config(text=str(res[2]))
+            window.update()
+        
 
-    root.geometry("1000x300")
-    lab1 = Label(root, text="Position 1", width=30)
-    lab1.pack()
-
-    lab2 = Label(root, text="Position 1", width=30)
-    lab2.pack()
-
-    lab3 = Label(root, text="Position 1", width=30)
-    lab3.pack()
-
-    spacer_lab = Label(root, text="", width=30)
-    spacer_lab.pack()
-
-    lab4 = Label(root, text="A to twoje karta!", width=30)
-    lab4.pack()
-
-    lab5 = Label(root, text="Position 1", width=30)
-    lab5.pack()
-
-    lab6 = Label(root, text="Position 1", width=30)
-    lab6.pack()
-    lab7 = Label(root, text="Position 1", width=30)
-    lab7.pack()
-
-    myEntry = Entry(root, width=30)
-    myEntry.pack()
-
-    myButton = Button(root, text="Send answer!", command=doNothingYet)
-    myButton.pack()
-
-    myConnectButton = Button(root, text="Connect me!", command=connectAndGetFirstCards)
-    myConnectButton.pack()
-
-    root.mainloop()
+    sck.close()
+    window.destroy()
 
 
-if __name__ == "__main__":
-    main()
+def getChatMessage(msg):
+
+    msg = msg.replace('\n', '')
+    texts = tkDisplay.get("1.0", tk.END).strip()
+
+    # enable the display area and insert the text and then disable
+    tkDisplay.config(state=tk.NORMAL)
+    if len(texts) < 1:
+        tkDisplay.insert(tk.END, "You->" + msg, "tag_your_message") # no line
+    else:
+        tkDisplay.insert(tk.END, "\n\n" + "You->" + msg, "tag_your_message")
+
+    tkDisplay.config(state=tk.DISABLED)
+
+    send_mssage_to_server(msg)
+
+    tkDisplay.see(tk.END)
+    tkMessage.delete('1.0', tk.END)
+
+
+def send_mssage_to_server(msg):
+    client_msg = str(msg)
+    client.send(client_msg.encode())
+    if msg == "exit":
+        client.close()
+        window.destroy()
+    print("Sending message")
+
+
+window.mainloop()
